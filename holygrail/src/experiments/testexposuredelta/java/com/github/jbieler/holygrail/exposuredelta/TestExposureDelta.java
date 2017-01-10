8f2b1918241543c63c6a183055f9ef61c47a0843
Jochen Bieler <jochen.bieler@web.de>
2017-01-10 17:01:35 +0100

    Add experiment that shows exposure value differences on images with delta EV 0.3

---
+++ holygrail/src/experiments/testexposuredelta/java/com/github/jbieler/holygrail/exposuredelta/TestExposureDelta.java
@@ -0,0 +1,41 @@
+package com.github.jbieler.holygrail.exposuredelta;
+
+import com.github.jbieler.holygrail.exposure.ExposureCalculation;
+import com.github.jbieler.holygrail.exposure.LuminanceExposureCalculation;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestExposureDelta {
+
+    @Test
+    @Ignore
+    public void testExposureDelta() throws IOException {
+        String[] filenames = new String[]{"000.jpg", "001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg",
+                "006.jpg", "007.jpg"};
+        List<Double> exposures = new ArrayList<Double>();
+        ExposureCalculation exposureCalculation = new LuminanceExposureCalculation();
+
+        for (String fname : filenames) {
+            exposures.add(exposureCalculation.exposureOf(testImage(fname)));
+        }
+
+        System.out.println("Exposures: " + exposures);
+
+        for (int i = 0; i < exposures.size() - 1; i++) {
+            System.out.println(exposures.get(i) - exposures.get(i + 1));
+        }
+    }
+
+    private BufferedImage testImage(String imageName) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(imageName).getFile());
+        return ImageIO.read(file);
+    }
+}
